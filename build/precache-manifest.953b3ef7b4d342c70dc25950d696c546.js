55101a9099e83857830004118a68df3e764e83f8
Official-Rahul <official.rahul.email@gmail.com>
2020-05-26 09:31:14 +0530

    build commit

---
+++ build/precache-manifest.953b3ef7b4d342c70dc25950d696c546.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d83bd66eb66e5c06a164ad06e71b46d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4ec9fdeb56f72919d33",
+    "url": "/static/css/main.178af8ec.chunk.css"
+  },
+  {
+    "revision": "cd4fb0117a3e46ebfb46",
+    "url": "/static/js/2.0a09d63f.chunk.js"
+  },
+  {
+    "revision": "fab1da7d8a32be3fe90e332131eacf28",
+    "url": "/static/js/2.0a09d63f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4ec9fdeb56f72919d33",
+    "url": "/static/js/main.54e7c48b.chunk.js"
+  },
+  {
+    "revision": "7aaf91a552c540c62644",
+    "url": "/static/js/runtime-main.a15bdacd.js"
+  }
+]);
